9d8e91ca6dfd0520234be61e509d09c16a2ae838
Tito <tatotyto99@gmail.com>
2025-05-14 14:28:52 +0200

    update obtenerRangos

--- php/obtenerRangos.php
+++ php/obtenerRangos.php
@@ -1,6 +1,6 @@
 <?php
 
-include __DIR__ . '/conexionBD.php';
+require __DIR__ . '/conexionBD.php';
 $consulta = 'SELECT * FROM juez;';
 $salida = array();
 if ($datos = $conexion->query($consulta)) {
